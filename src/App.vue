13c31007d15478004ef137a709a06197e261b749
Jelex <1170366657@qq.com>
2025-07-15 11:44:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { useRouteCacheStore } from '@/stores'
+
+useHead({
+  title: 'Vue3 Vant Mobile',
+  meta: [
+    {
+      name: 'description',
+      content: 'An mobile web apps template based on the Vue 3 ecosystem',
+    },
+    {
+      name: 'theme-color',
+      content: () => isDark.value ? '#00aba9' : '#ffffff',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
+    },
+  ],
+})
+
+const routeCacheStore = useRouteCacheStore()
+
+const keepAliveRouteNames = computed(() => {
+  return routeCacheStore.routeCaches
+})
+
+const mode = computed(() => {
+  return isDark.value ? 'dark' : 'light'
+})
+</script>
+
+<template>
+  <van-config-provider :theme="mode">
+    <nav-bar />
+    <router-view v-slot="{ Component }">
+      <section class="app-wrapper">
+        <keep-alive :include="keepAliveRouteNames">
+          <component :is="Component" />
+        </keep-alive>
+      </section>
+    </router-view>
+    <tab-bar />
+  </van-config-provider>
+</template>
+
+<style scoped>
+.app-wrapper {
+  width: 100%;
+  position: relative;
+  padding: 16px;
+}
+</style>
